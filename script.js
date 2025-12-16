5f437ffb32be767f5b96e488c8d7fb0a718d250f
Humna Ali <humnaali450@gmail.com>
2025-12-16 22:48:28 +0500

    init habit tracker project

---
+++ script.js
@@ -0,0 +1,9 @@
+// Habit Tracker – Day 1
+// Logic will be added on Day 2
+
+const habitInput = document.querySelector("input");
+const addHabitBtn = document.querySelector("button");
+const habitList = document.querySelector(".habit-list");
+
+// Just a test log to confirm JS is linked
+console.log("Habit Tracker JS connected");
