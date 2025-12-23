ff137c81a78d03f565b112293a5f6c7585c98e3f
Humna Ali <humnaali450@gmail.com>
2025-12-24 00:53:01 +0500

    save habits to localStorage

--- script.js
+++ script.js
@@ -2,14 +2,28 @@ const habitInput = document.getElementById("habitInput");
 const addHabitBtn = document.getElementById("addHabitBtn");
 const habitList = document.getElementById("habitList");
 
+let habits = JSON.parse(localStorage.getItem("habits")) || [];
+
+renderHabits();
+
 addHabitBtn.addEventListener("click", () => {
   const habitText = habitInput.value.trim();
 
   if (habitText === "") return;
 
-  const li = document.createElement("li");
-  li.textContent = habitText;
+  habits.push(habitText);
+  localStorage.setItem("habits", JSON.stringify(habits));
 
-  habitList.appendChild(li);
   habitInput.value = "";
+  renderHabits();
 });
+
+function renderHabits() {
+  habitList.innerHTML = "";
+
+  habits.forEach((habit) => {
+    const li = document.createElement("li");
+    li.textContent = habit;
+    habitList.appendChild(li);
+  });
+}
